ddc6d922766180b271cf00efab94d7a8b4874d9d
Dikai <dx@sustaera.com>
2022-09-13 17:22:28 -0400

    Publish settings

--- DacConfigManager.cs
+++ DacConfigManager.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using TwinCAT.Ads;
 using TwinCAT;
 using TwinCAT.TypeSystem;
 using TwinCAT.Ads.TypeSystem;
 using System.Xml.Serialization;
 using System.Xml;
-using System.Windows;
 
 namespace DacConfigManager
 {
